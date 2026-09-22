10d85253c7ecc8d2fb7a6dfc819ead4b3c2ba8af
Ayan <Ayan@LENOVO>
2026-09-23 00:50:44 +0400

    Add project files.

---
+++ lesson1/Program.cs
@@ -0,0 +1,49 @@
+﻿//task 1 
+//Console.WriteLine("Adinizi daxil edin: ");
+//string ad = Console.ReadLine();
+//Console.WriteLine("Yasinizi daxil edin: ");
+//int yash= int.Parse(Console.ReadLine());
+//Console.WriteLine("Boyunuzu daxil edin: ");
+//double boy = double.Parse(Console.ReadLine());
+//Console.WriteLine("Salam, menim adim "+ ad +"dir.Menim "+ yash+" yasim var,boyum "+ boy+"m-dir.");
+
+//task 2 
+//const double PI = 3.14159;
+//Console.WriteLine("Radiusu daxil edin: ");
+//double r = double.Parse(Console.ReadLine());
+//double S = PI * r * r;
+//Console.WriteLine("Dairenin sahesi: " + S);
+
+//task 4 
+//Console.WriteLine("1-ci qiymeti daxil edin: ");
+//string qiymet1 = Console.ReadLine();
+//Console.WriteLine("2-ci qiymeti daxil edin: ");
+//string qiymet2 = Console.ReadLine();
+//Console.WriteLine("3-cu qiymeti daxil edin: ");
+//string qiymet3 = Console.ReadLine();
+//Console.WriteLine("4-cu qiymeti daxil edin: ");
+//string qiymet4 = Console.ReadLine();
+//Console.WriteLine("5-ci qiymeti daxil edin: ");
+//string qiymet5 = Console.ReadLine();
+
+//int bal1 = int.Parse(qiymet1);
+//int bal2 = int.Parse(qiymet2);
+//int bal3 = int.Parse(qiymet3);
+//int bal4 = int.Parse(qiymet4);
+//int bal5 = int.Parse(qiymet5);
+
+//double ortaBal = (bal1 + bal2 + bal3 + bal4 + bal5) / 5;
+//Console.WriteLine("Orta bal: " + ortaBal);
+
+//if (ortaBal > 90)
+//{
+//    Console.WriteLine("Ela netice");
+//}
+//else if (ortaBal < 51)
+//{
+//    Console.WriteLine("Kesildiniz");
+//}
+//else if (ortaBal > 51 && ortaBal < 90)
+//{
+//    Console.WriteLine("Orta netice");
+//}
